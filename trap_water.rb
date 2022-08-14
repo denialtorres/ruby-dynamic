f6c633871dfa01e9482a2e33bb264c81c116dc1b
Daniel Torres <denial.torres@gmail.com>
2022-08-14 13:41:54 -0500

    refactor for trap_water

--- trap_water.rb
+++ trap_water.rb
@@ -13,31 +13,35 @@
 require 'pry'
 
 heights = [0,1,0,2,1,0,1,3,2,1,2,1]
+# heights = [3, 0, 2, 0, 4]
 total_volume = 0
 size = heights.size
-total_volumes = []
-trapped_water = 0
+total_volumes = 0
 
-heights.each_with_index do |height, index|
+for index in 0..size - 1
   left_max = 0
   right_max = 0
 
-  for a in 0..index do
-    left_max = [left_max, heights[a]].max
+  # for a in 0..index do
+  #   left_max = [left_max, heights[a]].max
+  # end
+  #
+  # for b in index..(size - 1) do
+  #   right_max = [right_max, heights[b]].max
+  # end
+
+  for i in 0..(size - 1) do
+    if i <= index
+      left_max = [left_max, heights[i]].max
+    end 
+    if i >= index
+      right_max = [right_max, heights[i]].max
+    end
   end
 
-  for b in index..height.size do
-    break if b > size - 1
-    right_max = [right_max, heights[b]].max
-  end
+  total_volume = [left_max, right_max].min - heights[index]
 
-  total_volume = [left_max, right_max].min - height
-
-  total_volumes.push(total_volume)
-
-  trapped_water = trapped_water + [left_max, right_max].min - height
+  total_volumes = total_volumes + total_volume
 end
 
-binding.pry
-
-total_volumes.inject(:+)
+puts total_volumes
